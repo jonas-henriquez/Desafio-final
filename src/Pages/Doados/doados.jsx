0838fa326abed956d6e489cb0a941ba7e628198b
jonas henrique <jonashenriquezz14@gmail.com>
2024-11-22 19:35:25 -0300

    commit inicial

---
+++ src/Pages/Doados/doados.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Doados(){
+    return(
+<section>
+    <h2>EU SOU A LIVROS DOADOS</h2>
+</section>
+    )
+}
---
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function QueroDoar(){
+return(
+    <section>
+        <h2>EU SOU A QUERO DOAR</h2>
+    </section>
+)
+
+
+}
